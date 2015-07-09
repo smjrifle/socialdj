6de1ff008a082893b0650b1e6dffa7981534d724
smjrifle <smjrifle@gmail.com>
2015-07-10 01:38:57 +0545

    Test

--- public/search.js
+++ public/search.js
@@ -1,26 +1,10 @@
 window.onload = function() {
 
     var messages = [];
-    var socket = io.connect('http://localhost:3700');
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
     var name = document.getElementById("search");
     
-    socket.on('message', function (data) {
-        // if(data.message) {
-        //     messages.push(data);
-        //     var html = '';
-        //     for(var i=0; i<messages.length; i++) {
-        //         html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
-        //         html += messages[i].message + '<br />';
-        //     }
-
-        //     content.innerHTML = html;
-        //     content.scrollTop = content.scrollHeight;
-        // } else {
-        //     console.log("There is a problem:", data);
-        // }
-    });
 
     sendButton.onclick = sendMessage = function() {
         if(name.value == "") {
@@ -30,7 +14,7 @@ window.onload = function() {
             gapi.client.load('youtube', 'v3', function() {
                searchByKeyword();
            });
-            
+
         }
     };
 }
