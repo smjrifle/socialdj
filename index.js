f4bdca8bdc0cc173116b4162454cc06e81bf5c09
greenspray <sudomksandwich@gmail.com>
2015-07-10 01:50:00 +0545

    second:

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ var express = require("express");
 var app = express();
 var port = 3700;
 
+
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
+
 app.engine('jade', require('jade').__express);
-app.get("/", function(req, res){
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+app.get("/chat", function(req, res){
     res.render("page");
 });
+
+
+app.post('/login',function(req, res){
+    console.log(res);
+});
+
 
 app.use(express.static(__dirname + '/public')); 
 
